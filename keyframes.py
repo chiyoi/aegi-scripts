dbad55a486f8798ded11d2d5f2812b759f2d257b
chiyoi <chiyoi2140@outlook.com>
2024-02-14 21:39:42 +0800

    Add keyframe count to extra output file

--- keyframes.py
+++ keyframes.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from moviepy.editor import VideoFileClip
 import numpy as np
@@ -5,26 +6,34 @@ import numpy as np
 import progress
 
 usage = '''Usage:
-python keyframes.py <video_path> > <save_path>
+python keyframes.py $VIDEO_PATH >$SAVE_PATH 3>$KEYFRAMES_FORMAT_SAVE_PATH
 '''
 difference = lambda frame1, frame2: round(np.sum(np.abs(frame1.astype(np.float32) - frame2.astype(np.float32))))
-threshold = 329844830
+threshold = 327150000
 
 if len(sys.argv) != 2:
   print(usage, file=sys.stderr)
   exit(1)
 video_path = sys.argv[1]
 
+try: extra_output = os.fdopen(3, 'w')
+except: extra_output = open(os.devnull, 'w')
+print('# keyframe format v1', file=extra_output)
+print('fps 0', file=extra_output)
+
 clip = VideoFileClip(video_path)
 total = round(clip.duration * clip.fps)
 prog = progress.initiate(total)
 last_frame = None
 last_timestamp = None
+frame_count = 0
 for timestamp, frame in clip.iter_frames(with_times=True):
   if last_frame is not None:
     if difference(frame, last_frame) >= threshold:
       print(round((timestamp + last_timestamp) / 2 * 1000), flush=True)
+      print(frame_count, file=extra_output, flush=True)
   last_frame = frame
   last_timestamp = timestamp
   prog = progress.update(prog)
+  frame_count += 1
 progress.cleanup()
